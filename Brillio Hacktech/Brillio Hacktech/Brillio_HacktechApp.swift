ef4ad4742fab4bddd12fe04287655ef0fb6c3f50
vladmarian20005 <78913181+vladmarian20005@users.noreply.github.com>
2024-10-25 16:41:22 +0300

    Initial Commit

---
+++ Brillio Hacktech/Brillio Hacktech/Brillio_HacktechApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brillio_HacktechApp.swift
+//  Brillio Hacktech
+//
+//  Created by Vlad Marian on 25.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Brillio_HacktechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
